529196f1051c597ec98c352e38d685c4db5063ae
Christoph Hombach <chris@dummy.de>
2022-12-21 19:23:36 +0100

    Refactored funcional tests

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from django.urls import resolve
 
 from lists.views import home_page
+from lists.modles import Item
 
 # Create your tests here.
 class HomePageTest(TestCase):
@@ -16,3 +17,13 @@ class HomePageTest(TestCase):
                                     data={'item_text' : 'A new list item'})
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
+"""
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) item'
+        first_item.save()
+
+        second_item = Item()
+"""
